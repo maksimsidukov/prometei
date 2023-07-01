d0cfbfd221f810c9f5b2c2dfce683e65a9e4b524
msidukov <maximsidukov@ya.ru>
2023-07-01 17:38:58 +0300

    main page

---
+++ assets/index.js
@@ -0,0 +1,22 @@
+(() =>  {
+    const inputs = document.querySelectorAll("input")
+    inputs.forEach(input => {
+        input.addEventListener('input', (ev) => {
+            if (ev.target.value.length) {
+                document.querySelector(`#${ev.target.id} + .input_label`).classList.add('input_label_active')
+            } else {
+                document.querySelector(`#${ev.target.id} + .input_label`).className = 'input_label'
+            }
+        })
+    })
+
+    document.addEventListener('scroll', (ev) => {
+        const scrollTop = ev.target.scrollingElement.scrollTop
+        const menu = document.querySelector('.menu_block')
+        if (scrollTop > 100) {
+            menu.style.backgroundColor = 'var(--blue)'
+        } else {
+            menu.style.backgroundColor = 'transparent'
+        }
+    })
+})()
